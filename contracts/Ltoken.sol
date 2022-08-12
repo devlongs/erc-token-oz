45798b5cd45e6a0a52f1e814ef242a3d7b526f20
Longs Pemun Gotar <developerlongs@gmail.com>
2022-08-12 23:11:04 +0000

    new file

--- contracts/Ltoken.sol
+++ contracts/Ltoken.sol
@@ -6,7 +6,7 @@ pragma solidity ^0.8.0;
 // import ERC20 base Contract from openzeppelin
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 
-contract LToken is ERC20 {
+contract LlToken is ERC20 {
     // We also want to call the constructor inside of ERC20 when our contructor is called.
     constructor(string memory _name, string memory _symbol)
         ERC20(_name, _symbol)
